64ca367dd503772c55eac339ef17fb87c40b6bd4
Boris Klymko <boris.klymko@gmail.com>
2021-03-21 16:25:08 +0000

    Installation de DockerInDocker et divers paquets

--- init/init.sh
+++ init/init.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
+set -e
+
 ROOT_DIR="$(cd "$(dirname "$0")"; pwd)"
 
-apt-get update --yes --quiet \
-&& apt-get --yes --quiet install procps bash-completion vim curl wget git openssh-client ansible \
-&& apt-get autoclean autoremove --yes --quiet
+apt-get --yes --quiet update
+apt-get --yes --quiet full-upgrade
+apt-get --yes --quiet install procps locales bash-completion vim curl wget ncat rsync man most git openssh-client ansible ansible-lint sshpass tree apt-transport-https ca-certificates gnupg
+apt-get --yes --quiet autoclean
+apt-get --yes --quiet autoremove
 
 cp -a "$ROOT_DIR/borx.sh"     /etc/profile.d/
 cp -a "$ROOT_DIR/vimrc.local" /etc/vim/
 
-sed -i '3iecho /etc/profile'           /etc/profile
-sed -i '2iecho /etc/profile.d/borx.sh' /etc/profile.d/borx.sh
-sed -i '2iecho /etc/bash.bashrc'       /etc/bash.bashrc
-sed -i '2iecho /root/.profile'         /root/.profile
-sed -i '2iecho /root/.bashrc'          /root/.bashrc
+sed -i '3iecho /etc/profile' /etc/profile
+for fic in /etc/profile.d/borx.sh /etc/bash.bashrc /root/.profile /root/.bashrc; do
+	sed -i "2iecho $fic" $fic
+done
+
+# dpkg-reconfigure tzdata && dpkg-reconfigure locales
+
+curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
 
