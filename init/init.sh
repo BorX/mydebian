fdf0ea88d5056e7ad4cf4969b80cee5475b8b90e
BorX <borx@f2.home>
2022-02-25 23:51:05 +0100

    Dockerfile : init via Ansible

--- init/init.sh
+++ init/init.sh
@@ -5,18 +5,14 @@ set -e
 ROOT_DIR="$(cd "$(dirname "$0")"; pwd)"
 
 apt-get --yes --quiet update
-apt-get --yes --quiet full-upgrade
-apt-get --yes --quiet install procps locales man bash-completion vim lsof git curl wget ncat jq jc pv most colordiff tree ncdu apt-transport-https ca-certificates gnupg rsync openssh-client ansible ansible-lint sshpass openjdk-11-jdk tmux
-apt-get --yes --quiet autoclean
-apt-get --yes --quiet autoremove
+
+apt-get --yes --quiet install ansible
+cd "$ROOT_DIR"
+ansible-playbook init-localhost.yml
 
 # dpkg-reconfigure tzdata && dpkg-reconfigure locales
 
 curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
-
-cp -a "$ROOT_DIR/borx.sh"     /etc/profile.d/
-cp -a "$ROOT_DIR/vimrc.local" /etc/vim/
-cp -a "$ROOT_DIR/gitconfig"   /root/.gitconfig
 
 sed -i '3iecho /etc/profile' /etc/profile
 for fic in /etc/profile.d/borx.sh /etc/bash.bashrc /root/.profile /root/.bashrc; do
