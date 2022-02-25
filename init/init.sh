407ebe7396fb34e411bf9ce45af1be8b2d743eb1
BorX <borx@f2.home>
2022-02-26 00:11:58 +0100

    Fichier borx.sh en mode éclaté

--- init/init.sh
+++ init/init.sh
@@ -15,6 +15,6 @@ ansible-playbook init-localhost.yml -e install_pkg_term=yes
 curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
 
 sed -i '3iecho /etc/profile' /etc/profile
-for fic in /etc/profile.d/borx.sh /etc/bash.bashrc /root/.profile /root/.bashrc; do
+for fic in /etc/profile.d/*.sh /etc/bash.bashrc /root/.profile /root/.bashrc; do
 	sed -i "2iecho $fic" $fic
 done
