c0cace3a727a473fa7694e3c3680606ea69f507a
Boris Klymko <boris.klymko@gmail.com>
2024-02-04 00:48:51 +0100

    helm

--- init/init.sh
+++ init/init.sh
@@ -10,16 +10,16 @@ apt-get --yes --quiet install ansible
 cd "$ROOT_DIR"
 ansible-playbook init-localhost.yml -e install_pkg_devops=true -e install_pkg_dev=true
 
-mkdir -p -m 755 /etc/apt/keyrings
-
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg   | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /usr/share/keyrings/kubernetes.gpg
+curl -fsSL https://baltocdn.com/helm/signing.asc                   | gpg --dearmor -o /usr/share/keyrings/helm.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main"   | tee /etc/apt/sources.list.d/google-cloud-sdk.list
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" >/etc/apt/sources.list.d/google-cloud-sdk.list
+echo 'deb [signed-by=/usr/share/keyrings/kubernetes.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /'         >/etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/usr/share/keyrings/helm.gpg arch=amd64] https://baltocdn.com/helm/stable/debian/ all main" >/etc/apt/sources.list.d/helm-stable-debian.list
 
 apt-get --yes --quiet update
-apt-get --yes --quiet install google-cloud-cli kubectl
+apt-get --yes --quiet install google-cloud-cli kubectl helm
 
 # dpkg-reconfigure tzdata && dpkg-reconfigure locales
 
